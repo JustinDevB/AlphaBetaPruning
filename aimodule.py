02ab405f8c83f229432f1205b3a004ebe5d4ae49
Justin <jbattles393@gmail.com>
2020-02-28 21:54:35 -0500

    Update aimodule.py

--- aimodule.py
+++ aimodule.py
@@ -15,35 +15,6 @@ squarevalue = [2, 1, 2, 1, 3, 1, 2, 1, 2]
 
 # pattern means row, column, diagonal
 
-def phase1(board, who):
-    moves = [x for x in range(9) if board[x] == 0]
-    return choice(moves)
-
-def phase2(board, who):
-    opp = 3 - who          # opp is the other player
-#
-# first check if we can win
-#
-    for pat in winners:
-        s = [board[x] for x in pat]    # values in the square for this pattern
-        if opp in s:                   # is the other play in this pattern
-            continue
-        if len([x for x in pat if board[x] == who]) == 2:
-            return pat[s.index(0)]
-#
-# next check if we can prevent the other player from winning
-#
-    for pat in winners:
-        s = [board[x] for x in pat]    # values in the square for this pattern
-        if who in s:                   # is the other play in this pattern
-            continue
-        if len([x for x in pat if board[x] == opp]) == 2:
-            return pat[s.index(0)]
-    for x in squareorder:
-        if board[x] == 0:
-            return x
-    print("Error in phase2: no available moves?", file=sys.stderr)
-    return -1
 
 def boardvalue(board, who):
     total = 0
@@ -67,20 +38,6 @@ def boardvalue(board, who):
 #
     total += sum([squarevalue[x] * board[x] for x in board])
     return total
- 
-def phase3(board, who):
-    bcopy = [-1 if x == 2 else x for x in board]  # copy board, change 2's to -1's
-    if who == 2:
-        who = -1
-    movelist = [x for x in range(9) if board[x] == 0]  # list of available moves
-    value = {}
-    for move in movelist:
-        bcopy[move] = who
-        value[move] = boardvalue(bcopy, who)
-        bcopy[move] = 0
-    fun = max if who == 1 else min                             # make fun either max or min
-    return fun(list(value.keys()), key = lambda x: value[x])   #find the best x based on value[x]
-
 
 # alpha-beta pruning version
 
@@ -124,7 +81,7 @@ def beta(board):     # considering O's moves
         board[move] = 0
     return bestmove, bestval
 
-def phase4(board, who):
+def alphabeta(board, who):
     global count
 
     if all([x == 0 for x in board]):
